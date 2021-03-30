306d07b75046682386fb742c5d6553a96d0a5584
DataS007 <81433147+DataS007@users.noreply.github.com>
2021-03-30 18:22:00 +0100

    Add files via upload

---
+++ exercises_8_1.py
@@ -0,0 +1,12 @@
+
+#CH 8 Exercices,1
+
+def chop():
+    list_names = list(['Paola', 'Sandra', 'Jose', 'Nacho', 'Patri'])
+    del list_names[4]
+    del list_names[0]
+    #list_namesd = list_names.pop(0)
+    #print(list_namesd)
+    print(list_names)
+
+chop()
