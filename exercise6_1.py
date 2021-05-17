e93b7ee80be24dbd0cf5318654a2f8718e0fc28e
Juan Rodriguez <81433147+DataS007@users.noreply.github.com>
2021-05-17 20:41:17 +0100

    Create exercise6_1.py
    Python for Everybody: Exploring Data Using Python 3 by Charles R. Severance,
    
    Exercise 6.1: Write a while loop that starts at the last character in the string and works its way backwards to the first character in the string, printing each letter on a separate line, except backwards.

---
+++ exercise6_1.py
@@ -0,0 +1,6 @@
+fruit = 'banana'
+index = len(fruit) - 1                    # Convert to index
+while index >= 0:
+    letter = fruit[index]
+    print(letter)
+    index -= 1                          # Update index
